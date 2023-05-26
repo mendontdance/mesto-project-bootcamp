8063d95427dce9f1ba4444a12dbf0dfbba9a4715
Danil Khegai <danilmaster95@gmail.com>
2023-05-26 08:35:23 +0300

    adding input text and name

--- index.js
+++ index.js
@@ -8,5 +8,13 @@ function popupEditOpenClose () {
     popupEdit.classList.toggle('popup_opened');
 }
 
-profileButtonEdit.addEventListener('click', popupEditOpenClose)
-popupClose.addEventListener('click', popupEditOpenClose)
+profileButtonEdit.addEventListener('click', popupEditOpenClose);
+popupClose.addEventListener('click', popupEditOpenClose);
+
+const profileName = document.querySelector('.profile__name');
+const profileText = document.querySelector('.profile__text');
+const popupInputName = document.querySelector('.popup__input_type_name');
+const popupInputText = document.querySelector('.popup__input_type_text');
+
+popupInputName.value= profileName.innerHTML;
+popupInputText.value = profileText.innerHTML;
